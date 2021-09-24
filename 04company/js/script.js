695971686f8a8cdebc67111c34d7dd534e3526ea
winiron <yamg1015@gmail.com>
2021-09-24 18:10:22 +0900

    2021-09-24-원격

--- 04company/js/script.js
+++ 04company/js/script.js
@@ -1,19 +1,16 @@
 $(document).ready(function(){
-    const slider1 = $(".slider").bxSlider( { controls:false } );
-    $(".sliderWrap .nextBtn").click( function(){ slider1.goToNextSlide(); } );
-    $(".sliderWrap .prevBtn").click( function(){ slider1.goToPrevSlide(); } );
+    const slider = $(".slider").bxSlider({
+        controls:false
+    });
+    $(".sliderWrap .nextBtn").click(function(){
+        slider.goToNextSlide();
+    });
+    $(".sliderWrap .prevBtn").click(function(){
+        slider.goToPrevSlide();
+    });
 
-    // 2021-09-16 작업
-    const slider2 = $(".slider2").bxSlider( { 
-        controls:false,
-        
-     } );
 
-    $(".slider2Wrap .nextBtn").click( function() { slider2.goToNextSlide(); } );
-    $(".slider2Wrap .prevBtn").click( function() { slider2.goToPrevSlide(); } );
-    
 
-    //toggle Button 영역
     // $(".toggle").on("이벤트명",리스너);
     // $(".toggle").on("click",function(){});
     // $(".toggle").click(function(){})
@@ -21,11 +18,11 @@ $(document).ready(function(){
         const toggle = $(".toggle").attr("class"); // value toggle
         console.log(toggle);
 
-        if( toggle == "toggle"){
+        if ( toggle == "toggle") {
             //$(".toggle").addClass("active");
             document.getElementsByClassName("toggle")[0].classList.add("active");
             $(".gnb").addClass("view");
-        }else{
+        } else {
             //$(".toggle").removeClass("active");
             document.getElementsByClassName("toggle")[0].classList.remove("active");
             $(".gnb").removeClass("view");
