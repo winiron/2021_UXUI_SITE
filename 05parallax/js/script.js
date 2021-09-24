33eb79a88fb070b2623397c34194e715f1199c49
winiron <yamg1015@gmail.com>
2021-09-24 15:09:47 +0900

    2021-09-24-원격

--- 05parallax/js/script.js
+++ 05parallax/js/script.js
@@ -4,12 +4,13 @@ let layer = document.querySelectorAll(".layer");
 window.addEventListener("scroll",function(){
     
     // console.log(scroll);
+    let scroll = window.pageYOffset;
 
     layer.forEach(function(item, index){
-        //data-speed 값 , 스크롤 값 =>  item의 style.trnaform
-        let scroll = window.pageYOffset;
+        // ①data-speed 값  ②스크롤 값 ⇨ item의 style.trnaform
         let speed = item.dataset.speed;
 
-        item.style.transform = `translateY(${scroll/speed*4}px)`;
+        // item : document.querySelectorAll("layer")[0]~[8]
+        item.style.transform = `translate3d(0,${scroll/speed*4}px,0)`;
     });
 });
