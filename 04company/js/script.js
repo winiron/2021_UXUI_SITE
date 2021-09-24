7733034605c146563184c606915e61bbfa4d2a39
winiron <yamg1015@gmail.com>
2021-09-24 14:37:29 +0900

    2021-09-24-원격

--- 04company/js/script.js
+++ 04company/js/script.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
-    const slider1 = $(".slider").bxSlider({
-        controls:false
-    });
-    $(".nextBtn").click(function(){
-        slider1.goToNextSlide();
-    });
-    $(".prevBtn").click(function(){
-        slider1.goToPrevSlide();
-    });
+    const slider1 = $(".slider").bxSlider( { controls:false } );
+    $(".sliderWrap .nextBtn").click( function(){ slider1.goToNextSlide(); } );
+    $(".sliderWrap .prevBtn").click( function(){ slider1.goToPrevSlide(); } );
 
+    // 2021-09-16 작업
+    const slider2 = $(".slider2").bxSlider( { 
+        controls:false,
+        
+     } );
+
+    $(".slider2Wrap .nextBtn").click( function() { slider2.goToNextSlide(); } );
+    $(".slider2Wrap .prevBtn").click( function() { slider2.goToPrevSlide(); } );
+    
+
+    //toggle Button 영역
     // $(".toggle").on("이벤트명",리스너);
     // $(".toggle").on("click",function(){});
     // $(".toggle").click(function(){})
