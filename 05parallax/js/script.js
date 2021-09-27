b8d1509600d116856fe745387ad7fe7beea1265b
winiron <yamg1015@gmail.com>
2021-09-27 10:31:57 +0900

    2021-09-27-원격

--- 05parallax/js/script.js
+++ 05parallax/js/script.js
@@ -8,7 +8,7 @@ window.addEventListener("scroll",function(){
 
     layer.forEach(function(item, index){
         // ①data-speed 값  ②스크롤 값 ⇨ item의 style.trnaform
-        let speed = item.dataset.speed;
+        let speed = item.dataset.speed; // div.layer data-speed 값
 
         // item : document.querySelectorAll("layer")[0]~[8]
         item.style.transform = `translate3d(0,${scroll/speed*4}px,0)`;
