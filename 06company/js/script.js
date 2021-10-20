43a21650cce10f874d4f3c9b65438dd7a678305d
winiron <yamg1015@gmail.com>
2021-10-20 09:04:16 +0900

    2021-10-19-Tue-PM-UPLOAD

--- 06company/js/script.js
+++ 06company/js/script.js
@@ -6,3 +6,34 @@ $(window).scroll(function(){
         $("#header").removeClass("fixed-top")
     }
 });
+
+
+// 포트폴리오 영역 ∴ isotope.js
+const btns = $("#portfolio .btn-group .btn");
+btns.click(function(){
+    let index = $(this).index(); 
+
+    btns.removeClass("active");
+    btns.eq(index).addClass("active");
+
+    let selector = btns.eq(index).attr("data-filter");
+    console.log(selector);
+
+    $("#portfolio .row").isotope({ filter:selector });
+});
+
+window.onload = function(){
+    $("#portfolio .btn-group #btn1").trigger("click");    
+};
+
+$("#portfolio .row .popup_link").magnificPopup({type:"image"});
+
+
+$("#customer .owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{items:1},
+        768:{items:2}
+    }
+});
