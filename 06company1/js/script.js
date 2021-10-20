026743660e59c754a8e77b7615691f5b5f3470b4
winiron <yamg1015@gmail.com>
2021-10-20 13:12:00 +0900

    2021-10-20-Wed-AM-UPLOAD

---
+++ 06company1/js/script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper-container ', {
+    // Optional parameters
+    loop: true,
+    slidesPerView:1,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-next',
+      prevEl: '.swiper-prev',
+    },
+
+  });
