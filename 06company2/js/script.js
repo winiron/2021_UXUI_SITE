792ac8e0ac619ce24302c931adf463c72814850f
winiron <yamg1015@gmail.com>
2021-10-28 17:03:48 +0900

    2021-10-28-THU-PM-UPDATE

--- 06company2/js/script.js
+++ 06company2/js/script.js
@@ -1,26 +1,39 @@
 $(window).scroll(function(){
-    let scrollY = $(window).scrollTop();
-
-    if(scrollY > 100) {
-        $("#header").addClass("fixed-top");
-    } else {
-        $("#header").removeClass("fixed-top");
-    }
+  let scrollY = $(window).scrollTop();
+  if(scrollY > 100) {
+    $("#header").addClass("fixed-top");
+  } else {
+    $("#header").removeClass("fixed-top");
+  }  
 });
 
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 30000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 30000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+$('#icon .owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  // nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      768:{
+          items:2
+      }
+  }
+})
